2229a2d9e7ca9b7d4817cfe204ef81ea04048c41
Saras <saras.me108@gmail.com>
2022-02-15 22:57:00 +0545

    model updated with defination attribute in flashcard table

--- flashcard/flashcode/models.py
+++ flashcard/flashcode/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Flashcard(models. Model):
     title = models.CharField(max_length=200)
+    defination = models.CharField(max_length=200  , null=True)    
     description = models.CharField(max_length=200 , null=True)
     remembred = models.BooleanField(default= False)
     date_created = models.DateTimeField(auto_now_add=True , null=True)
