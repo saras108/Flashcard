f17c7a06adac59831709275b3138b7036613396d
Saras <saras.me108@gmail.com>
2022-02-22 22:15:56 +0545

    Comments added for future use.

--- flashcard/uiux/views.py
+++ flashcard/uiux/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
+# data.html uses ajax to send a RESTFUL API request.
 def data(request):
     return render(request , 'frontend/data.html')
